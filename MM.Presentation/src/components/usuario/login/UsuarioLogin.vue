301ad3cdb5c691fbdf43286e23e968bd68a5019c
Matheus Oliveira <mso97@hotmail.com>
2020-09-18 19:21:21 -0300

    Cadastro de Token na web.

--- MM.Presentation/src/components/usuario/login/UsuarioLogin.vue
+++ MM.Presentation/src/components/usuario/login/UsuarioLogin.vue
@@ -1,8 +1,17 @@
 <template>
   <div class="UsuarioLogin">
         <form>
-            <v-text-field v-model="email" label="E-mail" required></v-text-field>
-            <v-text-field v-model="senha" label="Senha" required></v-text-field>
+            <v-text-field v-model="email" label="E-mail" 
+                @input="$v.email.$touch()" @blur="$v.email.$touch()"
+                :error-messages="errorsEmail"
+                required>
+            </v-text-field>
+            <v-text-field v-model="senha" label="Senha"
+                @input="$v.senha.$touch()" @blur="$v.senha.$touch()"
+                :error-messages="errorsSenha" 
+                required
+                type="password">
+            </v-text-field>
             <div class="row d-flex justify-center">
                 <v-btn @click="submit" large depressed>ENTRAR</v-btn>
             </div>
@@ -11,8 +20,35 @@
 </template>
 
 <script>
+    import axios from 'axios'
+    import { validationMixin } from 'vuelidate'
+    import { required, email } from 'vuelidate/lib/validators'
+
     export default {
         name: 'UsuarioLogin',
+
+        mixins: [validationMixin],
+
+        validations: {
+            email: {required, email},
+            senha: {required }
+        },
+
+        computed: {
+            errorsEmail () {
+                const errors = [];
+                if (!this.$v.email.$dirty) return errors;
+                !this.$v.email.required && errors.push('O e-mail é obrigatório.');
+                !this.$v.email.email && errors.push('É necessário um e-mail válido.');
+                return errors;
+            },
+            errorsSenha () {
+                const errors = [];
+                if (!this.$v.senha.$dirty) return errors;
+                !this.$v.senha.required && errors.push('A senha é obrigatória.');
+                return errors
+            }
+        },
 
         data: () => ({
             email: '',
@@ -21,7 +57,35 @@
 
         methods: {
             submit () {
-                alert("oi")
+                this.$v.$touch()
+                if(this.$v.$error) return
+
+                axios.post('login', this.$data)
+                    .then(response => {
+                        this.responseSubmit(response);
+                    })
+                    .catch(response => {
+                        this.responseSubmit(response);
+                    });
+            },
+
+            responseSubmit(response) {
+                if (response != null){
+                    if (response.data != null) {
+                        localStorage.setItem('NossoDinheiroTK', response.data.token);
+                        this.$toast.success("Login realizado.");
+                    }
+                    else {
+                        if (response.error != null){
+                            response.error.forEach(element => {
+                                this.$toast.warning(element.errorMessage);
+                            });
+                        }
+                        else {
+                            this.$toast.error(response.message);
+                        }
+                    }
+                }
             }
         }
     };
